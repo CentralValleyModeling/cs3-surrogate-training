fa7c1263265dea33e23c4d515b578ffe2e779c98
HamedZamanisabzi <Hamed.Zamanisabzi@water.ca.gov>
2026-01-09 10:41:05 -0800

    Update train.ipynb: split data by year (1923-1939 test, 1940-2015 train) and use 80/20 validation split

--- EC_estimator.py
+++ EC_estimator.py
@@ -145,7 +145,7 @@ def train_model(model, tensorboard_cb, X_train, y_train, X_test, y_test):
             tensorboard_cb
         ], 
         batch_size=128, 
-        epochs=1000, 
+        epochs=200, 
         verbose=0
     )
     return history, model
